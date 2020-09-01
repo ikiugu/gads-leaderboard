0d5fbb68025c972b7bab30a71f53aa4c4fc6d8a8
Alfred Ikiugu <ikiugualf@gmail.com>
2020-09-01 11:48:46 +0300

    splashscreen logic

--- app/src/main/java/com/ikiugu/leaderboard/SplashScreenActivity.java
+++ app/src/main/java/com/ikiugu/leaderboard/SplashScreenActivity.java
@@ -1,14 +1,29 @@
 package com.ikiugu.leaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    public static int SPLASH_SCREEN_TIME_OUT = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // use a timer to show the splash screen...frowned upon but it works 🤷‍♂️
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainActivityIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+
+                finish(); // to make sure this activity is not in the backstack
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
