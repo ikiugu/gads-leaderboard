ca63cd8a78cae53666764fa09ce8118ca7bd9796
Alfred Ikiugu <ikiugualf@gmail.com>
2020-09-14 10:56:41 +0300

    navigation to the submit activity

--- app/src/main/java/com/ikiugu/leaderboard/MainActivity.java
+++ app/src/main/java/com/ikiugu/leaderboard/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ikiugu.leaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -9,6 +12,8 @@ import com.google.android.material.tabs.TabLayout;
 import com.ikiugu.leaderboard.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        button = findViewById(R.id.buttonSubmit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent submitIntent = new Intent(MainActivity.this, SubmitActivity.class);
+                startActivity(submitIntent);
+            }
+        });
     }
 }
